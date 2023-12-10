51c2623ae0b8df4bfb64e2a2642b6b59975e21e9
dipanshuD09 <dipanshud146@gmail.com>
2023-12-10 17:06:05 +0530

    frontend complete
    dfsdg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from './components/Footer';
 
@@ -9,7 +10,7 @@ const App = () => {
     <Header />
     <main className='py-3'>
       <Container>
-        <h1>Welcome To EZcart</h1>
+        <Outlet />
       </Container>
     </main>
     <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 //import './index.css';
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Homescreen from './screens/Homescreen';
+import ProductScreen from './screens/ProductScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path='/' element={<Homescreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
