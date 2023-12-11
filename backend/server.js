aea41e7e52a97d5b2dc5b12c6d4b221d1b2192df
dipanshuD09 <dipanshud146@gmail.com>
2023-12-11 23:40:38 +0530

    setting up redux

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 connectDB();
 
@@ -15,13 +16,18 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// });
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+// app.get('/api/products/:id', (req, res) => {
+//     const product = products.find((p) => p._id === req.params.id);
+//     res.json(product);
+// });
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from 'react-router-dom';
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
+import "./assets/styles/bootstrap.custom.css";
 //import './index.css';
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Homescreen from './screens/Homescreen';
-import ProductScreen from './screens/ProductScreen';
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Homescreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path='/' element={<Homescreen />} />
-      <Route path='/product/:id' element={<ProductScreen />} />
-
+      <Route index={true} path="/" element={<Homescreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
 
 reportWebVitals();
